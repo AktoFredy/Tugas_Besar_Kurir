fc8a738a85c90a5cdc88f613fa13f6a506871f69
rAwrrrrrr1 <bet14A@gmail.com>
2022-09-22 20:50:56 +0700

    database users(Albert)

---
+++ app/src/main/java/com/example/tubes1/users/UserDao.kt
@@ -0,0 +1,17 @@
+package com.example.tubes1
+
+import androidx.room.*
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun addUser(user: User)
+    @Update
+    suspend fun updateUser(user: User)
+    @Delete
+    suspend fun deleteUser(user: User)
+    @Query("SELECT * FROM user")
+    suspend fun getUsers() : List<User>
+    @Query("SELECT * FROM user WHERE id =:user_id")
+    suspend fun getUser(user_id: Int) : List<User>
+}
