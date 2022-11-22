1f558656040f9602b97cd2052b2d8ba7caee8933
rAwrrrrrr1 <bet14a@gmail.com>
2022-11-22 21:42:08 +0700

    QR Code Scanner and Dependency + Layout

---
+++ app/src/main/java/com/example/tubes1/PengirimanDetailsQRScannerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tubes1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PengirimanDetailsQRScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengiriman_details_qrscanner)
+    }
+}
